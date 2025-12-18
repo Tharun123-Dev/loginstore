2d8c5373de522f4754ad0d15fea89db4263a8824
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-12-19 02:25:30 +0530

    new

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,21 +83,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-import os
+
 
 DATABASES = {
-    "default": {
-        "ENGINE": "mysql.connector.django",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
-        "OPTIONS": {
-            "autocommit": True,
-            "use_pure": True,
-        },
-    }
+  'default': {
+    'ENGINE': 'mysql.connector.django',
+    'NAME': os.getenv('DB_NAME'),
+    'USER': os.getenv('DB_USER'),
+    'PASSWORD': os.getenv('DB_PASSWORD'),
+    'HOST': os.getenv('DB_HOST'),
+    'PORT': os.getenv('DB_PORT'),
+    'OPTIONS': {'autocommit': True},
+  }
 }
 
 
