f5df4a2716e00b89e9cb14232c6b331fe7ca8b9b
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-12-19 02:20:36 +0530

    new

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,19 +83,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-        'OPTIONS': {
+    "default": {
+        "ENGINE": "mysql.connector.django",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+        "OPTIONS": {
             "autocommit": True,
-        }
+            "use_pure": True,
+        },
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
